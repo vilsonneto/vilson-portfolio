7125217d40911ea3520f5a130056ad63f10a0c0f
vilsonpneto <vilson.neto57@gmail.com>
2023-11-14 21:16:29 -0300

    feat: Add title

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -20,28 +20,32 @@ const Card = ({ project }: ICardProps) => {
         height={274}
         objectFit="contain"
       />
-    <a href={project.deploy} target="_blank">
-        <div className={style.info}>
-          <div className="ml-[10%] mr-[10%] mb-[13px] relative cursor-pointer">
-            <div>
-              <a href={project.github} target="_blank" className="hover:text-[#436db9]">
-                <FaGithub className="absolute right-0 top-[-120%] text-2xl" />
-              </a>
-            </div>
+      <div className={style.info}>
+        <div className="ml-[10%] mr-[10%] mb-[13px] relative cursor-pointer">
+          <div>
+            <a
+              href={project.github}
+              target="_blank"
+              className="hover:text-[#436db9]"
+            >
+              <FaGithub className="absolute right-0 top-[-120%] text-2xl" />
+            </a>
+          </div>
+          <a href={project.deploy} target="_blank">
             <h3 className={`m-3 text-2xl font-bold ${oswald.className}`}>
               {project.title}
             </h3>
-            <p className="m-3 text-xs">{project.description}</p>
-            <div className={`${style.boxTechs}`}>
-              {project.stack.map((tech) => (
-                <strong className={`${style.tech}`} key={crypto.randomUUID()}>
-                  {tech}
-                </strong>
-              ))}
-            </div>
+          </a>
+          <p className="m-3 text-xs">{project.description}</p>
+          <div className={`${style.boxTechs}`}>
+            {project.stack.map((tech) => (
+              <strong className={`${style.tech}`} key={crypto.randomUUID()}>
+                {tech}
+              </strong>
+            ))}
           </div>
         </div>
-      </a>
+      </div>
     </div>
   );
 };
