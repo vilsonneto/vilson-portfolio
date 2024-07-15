aa743a459abb88941b3684a1eea02319b7c1e21d
Vilson Neto <54651097+vilsonneto@users.noreply.github.com>
2024-07-15 12:10:46 -0300

    feat: add gtm tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,13 @@ export default function Document() {
     <Html lang="pt-Br">
       <Head>
         <Script id="google-analytics-script" strategy="lazyOnload">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PFBHKSJJ')`}
+        </Script>
+        <Script id="google-analytics-script" strategy="lazyOnload">
           {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
@@ -55,6 +62,11 @@ export default function Document() {
         <link rel="icon" href="/favicon.svg" type="image/svg" />
       </Head>
       <body>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PFBHKSJJ"
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}>
+          </iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>
