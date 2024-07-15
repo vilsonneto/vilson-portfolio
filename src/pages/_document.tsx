422196c43a5d6664b80b95ea02a8d09d6933ff9b
Vilson Neto <54651097+vilsonneto@users.noreply.github.com>
2024-07-15 12:13:53 -0300

    fix: altered strategy of script google-tag-manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
   return (
     <Html lang="pt-Br">
       <Head>
-        <Script id="google-analytics-script" strategy="lazyOnload">
+        <Script id="google-tag-manager" strategy="beforeInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
